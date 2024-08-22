0c1c8ff4e6fe796da76cdd9d138c36b55c2b4a6f
Rozario <sodienrozario@gmail.com>
2024-08-22 18:13:59 +0200

    most frontend work done (layout and stuff)

---
+++ ConversionMVC/ConversionMVC/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    var currentPageUrl = new URL(window.location.href);
+    $(".nav-pills li a").each(function () {
+        var linkUrl = new URL($(this).attr("href"), window.location.href);
+
+        if (currentPageUrl.pathname === linkUrl.pathname) {
+            $(this).addClass("active");
+        } else {
+            $(this).removeClass("active");
+        }
+    });
+})
